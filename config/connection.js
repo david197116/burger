97c7efeb46a2e465f8bb8141f7f1a3bffb2586cc
David Riveraperez <david.i.riveraperez@gmail.com>
2020-01-25 16:52:57 -0600

    modified config files added .env and .gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,24 @@
 var mysql = require("mysql");
 
-if(process.env.JAWSBD_URL){
-  connection = mysql.createConnection(process.env.JAWSBD_URL);
-  } else {
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
     connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "hacktheplanet",
-      database: "todoagain_db"
-
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: process.env.DB_PASS,
+        database: 'burgers_db'
     });
-  }
+}
+
+var mysql = require('mysql');
 
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+
 
 connection.connect(function(err) {
   if (err) {
